0fa7def617a690dc389357f4edd44d2f54974ec1
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2022-03-31 00:24:35 +0800

    Update scripts.js

--- project/scripts.js
+++ project/scripts.js
@@ -1,5 +1,26 @@
 
 
+function pic_selected(y)
+{
+    if (check_img(y) = true)
+    {
+        document.getElementById("score").innerHTML = "1/1: Nice, very based";
+        document.getElementById("real_tweet").src="frontend/images/real_tweet_reaction.png";
+        play();
+    } else //will only be true or false so no need to put error handling in
+    {
+        document.getElementById("score").innerHTML = "0/1: Kinda sus";
+        document.getElementById("fake_tweet").src="frontend/images/fake_tweet_reaction.png";
+        play();
+    }
+}
+
+function play() //play vineboom sound effect when selecting an answer
+{
+var audio = new Audio("/frontend/sounds/vine_boom.mp3");
+audio.play();
+}
+
 const img_array = [true, false] //boolean array containing values for each image
 
 function check_img(x)
