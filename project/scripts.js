972c7db1df097eefa00e8e07dcc918dd2790f73e
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2022-03-31 00:39:04 +0800

    Update scripts.js

--- project/scripts.js
+++ project/scripts.js
@@ -1,13 +1,19 @@
+const img_array = [true, false] //boolean array containing values for each image
 
+function check_img(x)
+{ 
+    return img_array[x]; //call the array and return value
+}
 
-function pic_selected(y)
+function pic_selected(x)
 {
-    if (check_img(y) = true)
+    if (check_img(x) = true)
     {
         document.getElementById("score").innerHTML = "1/1: Nice, very based";
         document.getElementById("real_tweet").src="frontend/images/real_tweet_reaction.png";
         play();
-    } else //will only be true or false so no need to put error handling in
+    } 
+        else //will only be true or false so no need to put error handling in
     {
         document.getElementById("score").innerHTML = "0/1: Kinda sus";
         document.getElementById("fake_tweet").src="frontend/images/fake_tweet_reaction.png";
@@ -17,13 +23,6 @@ function pic_selected(y)
 
 function play() //play vineboom sound effect when selecting an answer
 {
-var audio = new Audio("/frontend/sounds/vine_boom.mp3");
-audio.play();
-}
-
-const img_array = [true, false] //boolean array containing values for each image
-
-function check_img(x)
-{ 
-    return img_array[x]; //call the array and return value
+    var audio = new Audio("/frontend/sounds/vine_boom.mp3");
+    audio.play();
 }
