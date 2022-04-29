4dd11b5b638bd4e308223ef29965f7a5e1a90fdc
miltypig <83577568+miltypig@users.noreply.github.com>
2022-04-29 21:33:53 +0800

    Delete navBar.js

--- navBar.js
+++
@@ -1,8 +0,0 @@
-// This will be the main JS to control the event listeners attached to each navigation bar button
-
-// If Button pressed change display from flex to none and vice versa for the page to be displayed 
-
-// if leaderboards seleceted creates modal in the midal of the screen 
-
-// this java script will also handles eventlisteners for if the close button on the two pages is selected
-// returning back to the main page of the game 
