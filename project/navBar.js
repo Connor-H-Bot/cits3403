8af2307c66bc98ea5327dee11d8c0bc0f068cd0a
miltypig <83577568+miltypig@users.noreply.github.com>
2022-05-11 21:22:31 +0800

    Update navBar.js

--- project/navBar.js
+++ project/navBar.js
@@ -2,14 +2,14 @@
 
 // If Button pressed change display from flex to none and vice versa for the page to be displayed 
 
-// if leaderboards seleceted creates modal in the midal of the screen 
-
 // this java script will also handles eventlisteners for if the close button on the two pages is selected
 // returning back to the main page of the game 
 
 //this will not handle the reset game feature as it should be contained in its own JS function and POST statistics to be stored on web server.
+
 document.getElementById("helpB").addEventListener("click", setHelpPageON);
 document.getElementById("settingB").addEventListener("click", setSettingPageON);
+document.getElementById("exitButton1").addEventListener("click", returnToMain1);
 
 function setHelpPageON(){
     document.getElementById('mainScreen').classList.replace('Game', 'hidden');
@@ -21,4 +21,10 @@ function setSettingPageON(){
     document.getElementById('mainScreen').classList.replace('Game', 'hidden');
     document.getElementById('headerSelect').classList.add('hidden');
     document.getElementById('settingsScreen').classList.replace('settingsScreenOFF','settingsScreenON');
+}
+
+function returnToMain1(){
+    document.getElementById('helpScreen').classList.replace('helpScreenON','helpScreenOFF');
+    document.getElementById('headerSelect').classList.remove('hidden');
+    document.getElementById('mainScreen').classList.replace('hidden', 'Game');
 }
