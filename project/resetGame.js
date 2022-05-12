e8ffa8830378d64ba3317becb839830d04bbf04d
miltypig <83577568+miltypig@users.noreply.github.com>
2022-05-12 13:54:13 +0800

    Update resetGame.js

--- project/resetGame.js
+++ project/resetGame.js
@@ -3,3 +3,6 @@
 //post request to store current game and add to statistics.
 //if all tries are not used they are counted as fails. 
 
+//pull two new tweets and display 
+
+//reset counter according to difficulty setting 
